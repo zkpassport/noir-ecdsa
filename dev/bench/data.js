0a9d0841ee253b20c22436cd80317c880195a466
github-action-benchmark <github@users.noreply.github.com>
2026-09-14 11:26:28 +0000

    add ACIR Opcodes (customSmallerIsBetter) benchmark result for ad94905dc2b7800e70f09a9b412dfdfae4a190e8

---
+++ dev/bench/data.js
@@ -0,0 +1,107 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1789385188914,
+  "repoUrl": "https://github.com/zkpassport/noir-ecdsa",
+  "entries": {
+    "ACIR Opcodes": [
+      {
+        "commit": {
+          "author": {
+            "email": "75146596+Sarkoxed@users.noreply.github.com",
+            "name": "Sarkoxed",
+            "username": "Sarkoxed"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "ad94905dc2b7800e70f09a9b412dfdfae4a190e8",
+          "message": "Merge pull request #4 from zkpassport/as/noir-ecdsa-cleanup-fix\n\nchore:  rework and clean up ECDSA verification",
+          "timestamp": "2026-09-14T14:22:08+03:00",
+          "tree_id": "1ea5e8e810d05b5eb0872eb456838adab83034b3",
+          "url": "https://github.com/zkpassport/noir-ecdsa/commit/ad94905dc2b7800e70f09a9b412dfdfae4a190e8"
+        },
+        "date": 1789385187972,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "verify_brainpoolp192r1.json/main",
+            "value": 16310,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp192t1.json/main",
+            "value": 16310,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp224r1.json/main",
+            "value": 18806,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp224t1.json/main",
+            "value": 18806,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp256r1.json/main",
+            "value": 33672,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp256t1.json/main",
+            "value": 33672,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp384r1.json/main",
+            "value": 67831,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp384t1.json/main",
+            "value": 67831,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp512r1.json/main",
+            "value": 113982,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_brainpoolp512t1.json/main",
+            "value": 113982,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_secp192r1.json/main",
+            "value": 16310,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_secp224r1.json/main",
+            "value": 18806,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_secp256r1.json/main",
+            "value": 33672,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_secp384r1.json/main",
+            "value": 67831,
+            "unit": "acir_opcodes"
+          },
+          {
+            "name": "verify_secp521r1.json/main",
+            "value": 117073,
+            "unit": "acir_opcodes"
+          }
+        ]
+      }
+    ]
+  }
+}
