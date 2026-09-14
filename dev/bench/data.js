1289664a2a6507e278a2508cdaba97b5b06b382d
github-action-benchmark <github@users.noreply.github.com>
2026-09-14 11:35:32 +0000

    add Circuit Size (customSmallerIsBetter) benchmark result for 94ab77ed0cdc99309115fae383eb011af2bd0f48

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1789385730729,
+  "lastUpdate": 1789385732418,
   "repoUrl": "https://github.com/zkpassport/noir-ecdsa",
   "entries": {
     "ACIR Opcodes": [
@@ -223,6 +223,105 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/zkpassport/noir-ecdsa/commit/ad94905dc2b7800e70f09a9b412dfdfae4a190e8"
         },
         "date": 1789385190379,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "verify_brainpoolp192r1.json/main",
+            "value": 57245,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp192t1.json/main",
+            "value": 57245,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp224r1.json/main",
+            "value": 70041,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp224t1.json/main",
+            "value": 70041,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp256r1.json/main",
+            "value": 118430,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp256t1.json/main",
+            "value": 118430,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp384r1.json/main",
+            "value": 253066,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp384t1.json/main",
+            "value": 253066,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp512r1.json/main",
+            "value": 444267,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_brainpoolp512t1.json/main",
+            "value": 444267,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_secp192r1.json/main",
+            "value": 57245,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_secp224r1.json/main",
+            "value": 70041,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_secp256r1.json/main",
+            "value": 118430,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_secp384r1.json/main",
+            "value": 253066,
+            "unit": "circuit_size"
+          },
+          {
+            "name": "verify_secp521r1.json/main",
+            "value": 454979,
+            "unit": "circuit_size"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "75146596+Sarkoxed@users.noreply.github.com",
+            "name": "Sarkoxed",
+            "username": "Sarkoxed"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "94ab77ed0cdc99309115fae383eb011af2bd0f48",
+          "message": "Merge pull request #5 from zkpassport/as/update-readme\n\nchore: update README.md",
+          "timestamp": "2026-09-14T14:30:30+03:00",
+          "tree_id": "9d5e33b36d973a502ebb48eb5c5c3a3410b20164",
+          "url": "https://github.com/zkpassport/noir-ecdsa/commit/94ab77ed0cdc99309115fae383eb011af2bd0f48"
+        },
+        "date": 1789385732406,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
